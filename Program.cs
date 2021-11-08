c8f8606c952046ee67efcee6059ad1b1acfebf59
Ron Studený <ronstudeny@gmail.com>
2021-11-08 15:13:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ComplexNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class ComplexNum
+    {
+        int Im { get; set; }
+        int Re { get; set; }
+        public ComplexNum(int imNum, int reNum)
+        {
+            Im = imNum;
+            Re = reNum;
+        }
+
+        public ComplexNum AddNums(ComplexNum input)
+        {
+            ComplexNum res = new ComplexNum(Im, Re);
+            
+            res.Re += input.Re;
+            res.Im += input.Im;
+
+            return res;
+        }
+
+        public ComplexNum SubstractNums(ComplexNum input)
+        {
+            ComplexNum res = new ComplexNum(Im, Re);
+
+            res.Re -= input.Re;
+            res.Im -= input.Im;
+
+            return res;
+        }
+
+        public ComplexNum MultiplyNums(ComplexNum input)
+        {
+            ComplexNum res = new ComplexNum(Im, Re);
+
+            res.Re = (Re * input.Re) - (Im * input.Im);
+            res.Im = (Re * input.Im) + (Re * input.Im + input.Re * Im);
+
+            return res;
+        }
+
+        public override string ToString()
+        {
+            string res = $"{Re}  {Im}i ";
+
+            return res;
+        }
+    }
+}
